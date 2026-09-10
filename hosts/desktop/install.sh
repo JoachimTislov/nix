88f59021827882eded0786b6d5c5145a2a1379de
JoachimTislov <joachim.tislov@gmail.com>
2026-09-10 22:11:04 +0200

    Enable flakes in fresh installer environment

--- hosts/desktop/install.sh
+++ hosts/desktop/install.sh
@@ -16,4 +16,4 @@ if ! ping -c 1 -W 3 nixos.org >/dev/null 2>&1; then
   if [[ -n "$wifi" ]]; then read -r -s -p 'Wi-Fi password: ' password; printf '\n'; nmcli device wifi connect "$wifi" password "$password"; fi
 fi
 sudo nixos-generate-config --no-filesystems --show-hardware-config > "$root/hosts/desktop/hardware-configuration.nix"
-sudo nix run github:nix-community/disko/latest#disko-install -- --write-efi-boot-entries --flake "$root#desktop" --disk main "$disk"
+sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko/latest#disko-install -- --write-efi-boot-entries --flake "$root#desktop" --disk main "$disk"
