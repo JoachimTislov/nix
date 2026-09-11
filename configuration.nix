5534b77997cab48250118beecc321e3805ce1c97
JoachimTislov <joachim.tislov@gmail.com>
2026-09-11 21:59:45 +0200

    Pin flake inputs and configure Norwegian keyboard

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,8 @@
   nixpkgs.config.allowUnfree = true;
   time.timeZone = "Europe/Oslo";
   i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "no";
+  services.xserver.xkb.layout = "no";
   zramSwap = {
     enable = true;
     memoryPercent = 100;
